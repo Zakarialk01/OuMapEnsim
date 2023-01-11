36a99138874ba31e3829c2789612c106212bd52a
BachENSIM <ducbachng97@gmail.com>
2023-01-11 13:43:34 +0100

    display map/route - avatar move - try to update data

--- src/App.js
+++ src/App.js
@@ -3,27 +3,97 @@ import 'mind-ar/dist/mindar-image.prod.js';
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
 import './App.css';
-import MindARViewer from './mindar-viewer';
+import ModeViewAR from "./mindar-viewer";
 
 function App() {
   const [started, setStarted] = useState(false);
+  const [x, setX] = useState(1);
+  const [y, setY] = useState(1);
+  const [z, setZ] = useState(1);
+  const [oldX, setOldX] = useState(0);
+  const [oldY, setOldY] = useState(0);
+  const [oldZ, setOldZ] = useState(0);
+
+
+  //If any input changes reset
+  const basicReset = () => {
+    setStarted(true);
+   
+  };
+
+  //fonction pour mettre à jour les valeurs
+  const changeValue = () => {
+    // const { x1 } = old1.target.value;
+    // const { y1 } = old2.target.value;
+    // const { z1 } = old3.target.value;
+    // const { x2 } = new1.target.value;
+    // const { y2 } = new2.target.value;
+    // const { z2 } = new3.target.value;
+    // for (var i = 0; i < 10; i++) {
+    //   setOldX(i);
+    //   setOldY(i);
+    //   setOldZ(i);
+    //   i++;
+    //   setX(i++);
+    //   setY(i++);
+    //   setZ(i++);
+
+    //   console.log(`${oldX} ${oldY} ${oldZ} ; to: ${x} ${y} ${z};`);
+    //   //
+    // }
+    setOldX(-1.5);
+    setOldY(0);
+    setOldZ(0);
+    setX(-1.4);
+    setY(0);
+    setZ(0);
+   
+    basicReset();
+  };
+
   return (
     <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+      <h1>
+        Example React component with{" "}
+        <a href="https://github.com/hiukim/mind-ar-js" target="_blank">
+          MindAR
+        </a>
+      </h1>
 
       <div>
-	{!started && <button onClick={() => {setStarted(true)}}>Start</button>}
-	{started && <button onClick={() => {setStarted(false)}}>Stop</button>}
+        {/* {!started && <button onClick={() => {setStarted(true)}}>Start</button>} */}
+        {!started && <button onClick={changeValue}>Start</button>}
+        {started && (
+          <button
+            onClick={() => {
+              setStarted(false);
+             
+            }}
+          >
+            Stop
+          </button>
+        )}
       </div>
 
       {started && (
-	<div className="container">
-	  <MindARViewer/>
-	  <video></video>
-	</div>
+        <div className="container">
+          {/* <MindARViewer/> */}
+          <ModeViewAR
+            newdX={x}
+            newdY={y}
+            newdZ={z}
+            olddX={oldX}
+            olddY={oldY}
+            olddZ={oldZ}
+         
+           
+          />
+          <video></video>
+        </div>
       )}
     </div>
   );
 }
 
 export default App;
+
--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -1,32 +1,190 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from "react";
+import map from "./assets/myMap.jpg";
+import hellboy from "./assets/hellboy.glb";
+import flag from "./assets/flag.glb";
 
-export default () => {
+//cette fontionnalité va déclencher quand on active le mode AR et ça met à jour chaque 5s avec de nouvelles données (3 axes X,Y,Z)
+const ModeViewAR = (props) => {
   const sceneRef = useRef(null);
+  //6 properties old X,Y,Z and new X,Y,Z
+  const { newdX, newdY, newdZ, olddX, olddY, olddZ } = props;
 
+  const [x1, setX1] = useState(0);
+  const [y1, setY1] = useState(0);
+  const [z1, setZ1] = useState(0);
+
+  const [x2, setX2] = useState(0);
+  const [y2, setY2] = useState(0);
+  const [z2, setZ2] = useState(0);
+
+  var xx1 = -1.5;
+  var yy1;
+  var zz1;
+
+  let timer;
+
+  //cette fonction avec 3 pamamètres: newdX, newdY, newdZ
   useEffect(() => {
+    //le bouton switch entre 2 modes: AR et Map
     const sceneEl = sceneRef.current;
     const arSystem = sceneEl.systems["mindar-image-system"];
-    sceneEl.addEventListener('renderstart', () => {
-      arSystem.start(); // start AR 
-    });
+
+    setX1(props.olddX);
+    setY1(props.olddY);
+    setZ1(props.olddZ);
+
+    setX2(props.newdX);
+    setY2(props.newdY);
+    setZ2(props.newdZ);
+
+    sceneEl.addEventListener(
+      "renderstart",
+      () => {
+        arSystem.start();
+      },
+      [arSystem]
+    );
+
     return () => {
       arSystem.stop();
-    }
+      clearInterval(timer);
+    };
   }, []);
+  //ça fonctionne encore quand on coupe la caméra
+  // var litNumber = [{x:-1.5, y:0,z:0.1},
+  //   {x:-1.5, y:0,z:0.1},
+  //   {x:-1.25, y:-.015,z:0.1},
+  //   {x:-1.05, y:-0.35,z:0.1},
+  //   {x:-0.05, y:-0.75,z:0.1},
+  //   {x:0.15, y:-0.75,z:0.1},
+  //   {x:0.25, y:0.75,z:0.1},
+  //   {x:0.35, y:0.875,z:0.1},
+  //   {x:1.0, y:0.675,z:0.1}
+  // ];
+  useEffect(() => {
+    let incrementTime = 5000;
+    let timer = setInterval(() => {
+      var min = 0;
+      var max = 1;
+      var rand = min + Math.random() * (max - min);
+      var xx2 = -1.5;
+      //mettre à jour les values en utilisant state
+     
+      setX1(xx2);
+      // setY1(y1 + 0.01);
+      // setZ1(z1 + 0.01);
+      xx2 = xx2 + 0.1;
+      setX2(xx2);
+      xx2 = xx2 + 0.1;
+      // setY2(y1 + 0.02);
+      // setZ2(z1 + 0.02);
+
+      console.log(`after 5s x1 ${x1} ${y1} ${z1} ${rand}`);
+      console.log(`after 5s x2 ${x2} ${y1} ${z1}`);
+
+      //garder des valeurs anciennes
+
+      //mettre à jour les values en utilisant state
+    }, incrementTime);
+   
+    // return () => clearInterval(timer);
+
+  }, [x1, y1, z1, x2, y2, z2]);
+
+  var speed = 5000;
 
   return (
-    <a-scene ref={sceneRef} mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+    <a-scene
+      ref={sceneRef}
+      mindar-image="imageTargetSrc: mundo.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+      color-space="sRGB"
+      embedded
+      renderer="colorManagement: true, physicallyCorrectLights"
+      vr-mode-ui="enabled: false"
+      device-orientation-permission-ui="enabled: true"
+    >
       <a-assets>
-	<img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.png" />
-	<a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
-      </a-assets>
+        <img id="card" src={map} />
 
+        <a-asset-item id="avatarModel" src={hellboy}></a-asset-item>
+        <a-asset-item id="flagModel" src={flag}></a-asset-item>
+      </a-assets>
       <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
+      {/* une scène pour dessiner les points intérêts sur la carte  */}
+
+      {/* une scène pour afficher les étudiant */}
       <a-entity mindar-image-target="targetIndex: 0">
-	<a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-	<a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
+        <a-plane
+          src="#card"
+          position="0 0 0"
+          height="2"
+          width="3"
+          rotation="0 0 0"
+        ></a-plane>
+
+       {/* afficher les flags pour déterminer des points intérets */}
+        <a-gltf-model
+          rotation="0 270 0 "
+          position="-1.5 0 0.1"
+          scale="0.3 0.3 0.3"
+          src="#flagModel"
+          color="red"
+        ></a-gltf-model>
+
+        <a-gltf-model
+          rotation="0 270 0 "
+          position="0.2 -0.75 0.1"
+          scale="0.3 0.3 0.3"
+          src="#flagModel"
+        ></a-gltf-model>
+
+        <a-gltf-model
+          rotation="0 270 0 "
+          position="1.5 0.75 0.1"
+          scale="0.3 0.3 0.3"
+          src="#flagModel"
+        ></a-gltf-model>
+
+        {/* Pour afficher le trajet à réaliser par des  */}
+        <a-entity 
+          line="start: -1.5 0 0.1; end: -1.25 -0.15 0.1; color: red;"
+          line__2="start: -1.25 -0.15 0.1; end: -1.05 -0.35 0.1; color: red"
+          line__3="start: -1.05 -0.35 0.1; end: -0.05 -0.75 0.1; color: red"
+          line__4="start: -0.05 -0.75 0.1; end: 0.15 -0.75 0.1; color: red"
+          line__5="start:  0.15 -0.75 0.1; end: 0.25 0.75 0.1; color: red"
+          line__6="start: 0.25 0.75 0.1; end: 0.35 0.875 0.1; color: red"
+          line__7="start: 0.35 0.875 0.1; end: 1.0 0.675 0.1; color: red"
+          line__8="start: 1.0 0.675 0.1; end: 1.5 0.675 0.1; color: red"
+          >
+        </a-entity>
+
+
+        {/* perso 1 */}
+        <a-gltf-model
+          rotation="0 0 0 "
+          position="-1 0 0.5"
+          scale="0.3 0.3 0.3"
+          src="#avatarModel"
+          animation={`property: position; from: ${x1} ${y1} ${z1} ; to: ${x2} ${y2} ${z2};dur:${speed}; easing: easeInOutQuad; loop: true; dir: alternate`}
+        ></a-gltf-model>
+
+        {/* perso 2 */}
+        {/* <a-gltf-model
+          rotation="0 0 0 "
+          position="-1 0 0.1"
+          scale="0.2 0.2 0.2"
+          src="#avatarModel"
+          animation={`property: position; from: ${x1 + 0.1} ${y1 + 0.01} ${
+            z1 + 0.01
+          } ; to: ${x2 + 0.01} ${y2 + 0.01} ${
+            z2 + 0.01
+          };dur:4000; easing: easeInOutQuad; loop: true; dir: alternate`}
+          // animation={`property: position; from: -1 0 0.5; to: 0 1.25 0.95;dur:4000; easing: easeInOutQuad; loop: true; dir: alternate`}
+        ></a-gltf-model> */}
       </a-entity>
     </a-scene>
-  )
-}
+  );
+};
+
+export default ModeViewAR;
